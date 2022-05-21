2561abb2118c72a3961b1ecc88a797e6f29b5ab7
zigm <77489316+orgxtopaz@users.noreply.github.com>
2022-05-21 16:03:01 +0800

    Update viewSalaryRequest_controller.js

--- controller/viewSalaryRequest_controller.js
+++ controller/viewSalaryRequest_controller.js
@@ -3,7 +3,7 @@
 let salaryRequests = require("../models/salaryRequests.model");
 
 const displaySalaryRequest =async (req, res) => {
-
+const sort = { _id: -1 };
 ////DISPLAY ALL TRAVEL LOGS WITH THE SPECIFIC OFFICIAL
 
     const position = req.body.position;
@@ -14,7 +14,7 @@ const displaySalaryRequest =async (req, res) => {
       { requestTo: { $eq: position } }
    
   
-    ) // PROMISE IF ELSE
+    ).sort(sort) // PROMISE IF ELSE
       
       .then((requestLog) => res.status(200).json(requestLog)) // IF TRUE CHECK
       .catch((err) => res.status(400).json("Error : " + err)); // IF ERROR
