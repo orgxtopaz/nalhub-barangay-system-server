26cb5af31d7f9f899b812e5d0406491b1f8b236e
zigm <77489316+orgxtopaz@users.noreply.github.com>
2022-05-21 16:05:33 +0800

    Update displayAttendance_controller.js

--- controller/displayAttendance_controller.js
+++ controller/displayAttendance_controller.js
@@ -7,14 +7,14 @@ const displayAttendance =async (req, res) => {
 
 ////DISPLAYING THE ATTENDACE SPECIFIC OFFICIAL
     const email = req.body.email;
-  
+  const sort = { _id: -1 };
   
     Attendance.find(  
   
       { email: { $eq: email } }
    
   
-    ) // PROMISE IF ELSE
+    ).sort(sort) // PROMISE IF ELSE
       
       .then((attendance) => res.status(200).json(attendance)) // IF TRUE CHECK
       .catch((err) => res.status(400).json("Error : " + err)); // IF ERROR
