f39da4629f4d477dab19f033c61f49169e3eb9b9
zigm <77489316+orgxtopaz@users.noreply.github.com>
2022-05-21 15:54:41 +0800

    Update viewSpecificTravelLog_controller.js

--- controller/viewSpecificTravelLog_controller.js
+++ controller/viewSpecificTravelLog_controller.js
@@ -3,7 +3,8 @@ let travelLog = require("../models/travellogs.model");
 
 const viewSpecificTravelLog =async (req, res) => {
 //VIEW SPECIFIC TRAVEL LOGS DETAILS OF OFFICIALS
-travelLog.findById(req.params.id)
+
+travelLog.findById(req.params.id).sort(sort) 
 .then((user) => res.json(user))
 .catch((err) => res.status(400).json("Error: " + err));
 }
