c8f6fd7c36c5740025ea5dc080b56aad7a420b7a
zigm <77489316+orgxtopaz@users.noreply.github.com>
2022-05-21 16:01:52 +0800

    Update viewTotalAttendance_controller.js

--- controller/viewTotalAttendance_controller.js
+++ controller/viewTotalAttendance_controller.js
@@ -1,7 +1,8 @@
 let totalAttendance = require("../models/totalAttendance.model");
 
 const viewTotalAttendance =async (req, res) => {
-    totalAttendance.find() // PROMISE IF ELSE
+      const sort = { _id: -1 };
+    totalAttendance.find().sort(sort)  // PROMISE IF ELSE
     .then((totalAttendance) => res.json(totalAttendance)) // IF TRUE CHECK
     .catch((err) => res.status(400).json("Error : " + err)); // IF ERROR
 
