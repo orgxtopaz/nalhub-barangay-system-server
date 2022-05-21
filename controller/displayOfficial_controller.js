b812fb139ee617e182df3706c854234f2481b7fa
zigm <77489316+orgxtopaz@users.noreply.github.com>
2022-05-21 16:05:07 +0800

    Update displayOfficial_controller.js

--- controller/displayOfficial_controller.js
+++ controller/displayOfficial_controller.js
@@ -2,7 +2,9 @@
 let User = require("../models/user.model");
 
 const displayOfficial =async (req, res) => {
-    User.find() // PROMISE IF ELSE
+        const sort = { _id: -1 };
+
+    User.find().sort(sort) // PROMISE IF ELSE
     .then((user) => res.json(user)) // IF TRUE CHECK
     .catch((err) => res.status(400).json("Error : " + err)); // IF ERROR
 
